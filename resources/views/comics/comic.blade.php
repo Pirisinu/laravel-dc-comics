c04f72d1fb1f7df699567eaac3c38e80cc2bee5d
Pirisinu <palazzialessandro2@gmail.com>
2023-11-23 18:26:08 +0100

    [ add ]Route::id

--- resources/views/comics/comic.blade.php
+++ resources/views/comics/comic.blade.php
@@ -4,15 +4,19 @@
 
 
 
-    <div class="container">
-            <div class="card" style="width: 18rem;">
-                <img class="card-img-top w-100 h-75 object-fit-cover " src="{{$comic->thumb}}" alt="Card image cap">
-                <div class="card-body p-0">
-                    <h5 class="card-title">{{$comic->title}}</h5>
-                    <p class="card-text">{{$comic->price}}</p>
+    <div class="container d-flex">
+
+                <img class="w-100 w-75 object-fit-cover " src="{{$comic->thumb}}" alt="Card image cap">
+                <div>
+                    <h5 class="card-title"><strong>Titolo: </strong>{{$comic->title}}</h5>
+                    <p class="card-text"><strong>Descrizione: </strong>{{$comic->description}}</p>
+                    <p class="card-text"><strong>Prezzo: </strong>{{$comic->price}}</p>
+                    <p class="card-text"><strong>Serie: </strong>{{$comic->series}}</p>
+                    <p class="card-text"><strong>Giorno prima vendita: </strong>{{$comic->sale_date}}</p>
+                    <p class="card-text"><strong>Genere: </strong>{{$comic->type}}</p>
+                    <a href="{{route('comics.index')}}">Home</a>
                 </div>
-            </div>
-            @endforeach
+
     </div>
 
 
