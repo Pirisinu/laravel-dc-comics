6f330b9ee689e3a529c5ebca22606ef0bdabbf02
Pirisinu <palazzialessandro2@gmail.com>
2023-11-23 16:11:24 +0100

    [ init ] repo

---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,8 @@
+<header>
+    <nav class="nav bg-black ">
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+        <a class="nav-link" href="#">Link</a>
+      </nav>
+</header>
